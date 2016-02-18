8f51d31cd27a1e8202b41b90d4250de60e85bc70
Matt Swain <m.swain@me.com>
2016-02-18 23:33:04 +0000

    Ajax poll instead of refresh on online demo

--- cdeweb/static/js/results.js
+++ cdeweb/static/js/results.js
@@ -1,4 +1,21 @@
 $(function() {
 
+    function getResults() {
+        if ($('.progress').length > 0) {
+            $.get('/api/job/' + jobId, function(data) {
+                console.log(data);
+                if (data.status == 'SUCCESS') {
+                    window.location.reload();
+                } else {
+                    setTimeout(function () {
+                        getResults();
+                    }, 1000);
+                }
+            });
+        }
+    }
+
+    // Poll to check if job has completed
+    getResults();
 
 });
